d6a337af99f82ae9f7aeab4d24f3bc16ef63eba7
Vladimir Kuptsov <vkuptcov@gmail.com>
2016-10-11 14:49:53 +0200

    + link to the original source code

--- src/main/scala/eu/inn/gc/GCInspector.scala
+++ src/main/scala/eu/inn/gc/GCInspector.scala
@@ -1,36 +1,10 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *//*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/**
+  * This code idea taken from the org.apache.cassandra.service.GCInspector class
+  * in the Apache Cassandra project.
+  *
+  * You could see the original source code in
+  * https://git1-us-west.apache.org/repos/asf?p=cassandra.git;a=blob;f=src/java/org/apache/cassandra/service/GCInspector.java
+  */
 package eu.inn.gc
 
 import com.sun.management.{GarbageCollectionNotificationInfo, GcInfo}
