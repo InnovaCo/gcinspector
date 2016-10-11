362af292415450bb09203f65ab53c961346c5cbe
Vladimir Kuptsov <vkuptcov@gmail.com>
2016-10-11 15:39:08 +0200

    * fix travis build

--- travis/build.sh
+++ travis/build.sh
@@ -3,7 +3,7 @@ set -e
 
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "master" ]]; then
   if grep version "build.sbt" | grep -q "\-SNAPSHOT"; then
-    sbt +test +publish
+    sbt test publish
   else
     openssl aes-256-cbc -k "$key_password" -in ./travis/inn-oss-public.enc -out ./inn-oss-public.asc -d
     openssl aes-256-cbc -k "$key_password" -in ./travis/inn-oss-private.enc -out ./inn-oss-private.asc -d
